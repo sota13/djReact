d05e16142da0318880f6a32eca04f85cfbcb344a
mohammed <mohad.sota@gmail.com>
2021-03-13 17:39:55 +0300

    the frontend par =t for the project

---
+++ frontend/gui/src/containers/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class CustomLayout  extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <p>hello</p>
+         );
+    }
+}
+ 
+export default CustomLayout ;
